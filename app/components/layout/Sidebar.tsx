9e14191f3bb5b122466cb561a99da0fe9bc068c3
zamanshahed <sumonshd@gmail.com>
2025-11-07 00:33:25 +0600

    FEAT: sidebar ready

---
+++ app/components/layout/Sidebar.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+export default function Sidebar() {
+    return (
+        <div className="absolute top-[108px] left-6 z-50 w-[72px] max-h-[calc(100vh-120px)] no-scrollbar overflow-y-auto overflow-x-clip border rounded-2xl px-4 py-6 mb-5 space-y-4">
+            <Image
+                src={"/images/icons/dashboard.svg"}
+                alt="dashboard-icon"
+                width={40}
+                height={40}
+            />
+            <SidebarItem iconPath={"/images/icons/mortarboard-02.svg"} />
+            <SidebarItem iconPath={"/images/icons/marketing.svg"} />
+            <SidebarItem iconPath={"/images/icons/bars.svg"} />
+            <SidebarItem iconPath={"/images/icons/message-02.svg"} />
+            <SidebarItem iconPath={"/images/icons/bars-02.svg"} />
+            <SidebarItem iconPath={"/images/icons/user-group.svg"} />
+            <SidebarItem iconPath={"/images/icons/calendar-03.svg"} />
+
+            <div className="h-px bg-black/20" />
+
+            <SidebarItem iconPath={"/images/icons/analytics-up.svg"} />
+            <SidebarItem iconPath={"/images/icons/money-bag-02.svg"} />
+            <SidebarItem iconPath={"/images/icons/ranking.svg"} />
+
+            <div className="h-px bg-black/20" />
+
+            <SidebarItem iconPath={"/images/icons/customer-support.svg"} />
+            <SidebarItem iconPath={"/images/icons/user-group.svg"} />
+            <SidebarItem iconPath={"/images/icons/search.svg"} />
+        </div>
+    );
+}
+
+function SidebarItem({ iconPath }: { iconPath: string }) {
+    return (
+        <div className="w-10 h-10 flex items-center justify-center">
+            <Image src={iconPath} alt="sidebar icon" width={20} height={20} />
+        </div>
+    );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,36 +2,40 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Topbar from "./components/layout/Topbar";
+import Sidebar from "./components/layout/Sidebar";
 
 const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
+    variable: "--font-inter",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Next Athlete",
-  description: "Crafted by zamanShahed",
-  icons: {
-    icon: "/favicon.png",
-  },
+    title: "Next Athlete",
+    description: "Crafted by zamanShahed",
+    icons: {
+        icon: "/favicon.png",
+    },
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${inter.variable} antialiased bg-[#E7F2F5]`}
-        style={{
-          fontFamily: inter.style.fontFamily,
-        }}
-      >
-        <Topbar/>
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.variable} antialiased bg-[#E7F2F5]`}
+                style={{
+                    fontFamily: inter.style.fontFamily,
+                }}
+            >
+                {/* <div className="min-h-[1200px] mb-20"> */}
+                    <Topbar />
+                    <Sidebar />
+                {/* </div> */}
+                {children}
+            </body>
+        </html>
+    );
 }
