c37728de6eecd4d2ff782b526f223bebb8db8fd5
zamanshahed <sumonshd@gmail.com>
2025-11-07 21:00:04 +0600

    WIP: announcement card

---
+++ app/components/cards/AnnouncementCard.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AnnouncementCard() {
+  return (
+    <div className='h-[450px] w[600px] bg-white rounded-2xl p-4'>AnnouncementCard</div>
+  )
+}
--- app/components/ui/AnnouncementSection.tsx
+++ app/components/ui/AnnouncementSection.tsx
@@ -1,17 +1,21 @@
 import Button from "../buttons/Button";
 import GlassCard from "../cards/GlassCard";
+import AnnouncementCard from "../cards/AnnouncementCard";
 
 export default function AnnouncementSection() {
     return (
         <div className="w-full relative">
             <GlassCard>
-                <div className="h-[300px]">
+                <div className="h-[50px]">
                     <div className="flex items-center justify-between bg-white/30 border-b border-white/30 w-full rounded-t-2xl px-5 py-4 absolute left-0 top-0">
                         <h1 className="text-xl font-bold">
                             Announcements preview
                         </h1>
                         <Button>View More</Button>
                     </div>
+                </div>
+                <div className="pt-6">
+                    <AnnouncementCard />
                 </div>
             </GlassCard>
         </div>
